6302d1e928d5df341918f7c3bd97175c42c988db
reza <reza.abdollahzadeh12@gmail.com>
2021-07-23 23:44:40 +0430

    search Activity added

---
+++ app/src/main/java/com/mobiledevelopment/actex/views/SearchResultAdapter.java
@@ -0,0 +1,67 @@
+package com.mobiledevelopment.actex.views;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.mobiledevelopment.actex.R;
+import com.mobiledevelopment.actex.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class SearchResultAdapter extends ArrayAdapter<Movie> {
+    private String imageBaseUrl = "https://image.tmdb.org/t/p/w92";
+
+    private OnMovieClickListener listener;
+
+    public void setOnClickListener(OnMovieClickListener listener) {
+        this.listener = listener;
+    }
+
+
+    private static class ViewHolder {
+        public ImageView ivCover;
+        public TextView tvTitle;
+        public TextView tvReleaseDate;
+    }
+
+    public SearchResultAdapter(@NonNull Context context, ArrayList<Movie> arrMovie) {
+        super(context, 0, arrMovie);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+//        final Movie movie = getItem(position);
+//        final ViewHolder viewHolder;
+//        if (convertView == null) {
+//            viewHolder = new ViewHolder();
+//            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            convertView = inflater.inflate(R.layout.item_search_res, parent, false);
+//            viewHolder.ivCover = (ImageView) convertView.findViewById(R.id.ivMovieCover);
+//            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+//            viewHolder.tvReleaseDate = (TextView) convertView.findViewById(R.id.tvReleaseDate);
+//            convertView.setTag(viewHolder);
+//        } else {
+//            viewHolder = (ViewHolder) convertView.getTag();
+//        }
+//        viewHolder.tvTitle.setText(movie.getTitle());
+//        viewHolder.tvReleaseDate.setText(movie.getReleaseDate());
+//        convertView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                listener.onMovieClick(movie);
+//            }
+//        });
+//        Picasso.get().load(imageBaseUrl+movie.getPosterPath()).placeholder(R.drawable.loading_placeholder).error(R.drawable.ic_baseline_image_24).into(viewHolder.ivCover);
+//        return convertView;
+        return new Button(getContext()); // TODO
+    }
+}
---
+++ app/src/main/java/com/mobiledevelopment/actex/views/SearchableActivity.java
@@ -0,0 +1,109 @@
+package com.mobiledevelopment.actex.views;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+import com.mobiledevelopment.actex.MainActivity;
+import com.mobiledevelopment.actex.R;
+import com.mobiledevelopment.actex.clients.MovieListsApiEndpointInterface;
+import com.mobiledevelopment.actex.clients.RetrofitBuilder;
+import com.mobiledevelopment.actex.models.Movie;
+import com.mobiledevelopment.actex.models.lists.MovieList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    private static final String TAG = "SEARCHABLE_ACTIVITY";
+    private ListView searchResultLv;
+    private SearchResultAdapter adapter;
+    private Toolbar toolbar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_searchable); // TODO
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Log.i(TAG, "onCreate: ");
+//        searchResultLv = findViewById(R.id.search_result_lv);
+//        adapter = new SearchResultAdapter(this, new ArrayList<Movie>());
+//        adapter.setOnClickListener(movie -> {
+//            Log.i(TAG, "onItemClick: " + movie);
+//            Intent intent = new Intent(SearchableActivity.this, MainActivity.class);
+//            intent.putExtra("searchRes", movie);
+//            startActivity(intent);
+//        });
+//        searchResultLv.setAdapter(adapter);
+//        Intent intent = getIntent();
+//        handleIntent(intent); // TODO
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.i(TAG, "onNewIntent: ");
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        Log.i(TAG, "handleIntent: ");
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            searchQuery(query);
+        }
+    }
+
+    private void searchQuery(String query) {
+        MovieListsApiEndpointInterface movieListApi = RetrofitBuilder.getMovieApi();
+        Call<MovieList> movieList = movieListApi.getSearchResults(getResources().getString(R.string.api_key), query);
+        movieList.enqueue(new Callback<MovieList>() {
+            @Override
+            public void onResponse(Call<MovieList> call, Response<MovieList> response) {
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        List<Movie> movies = response.body().results;
+                        adapter.clear();
+                        for (Movie movie : movies) {
+                            adapter.add(movie);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                } else Log.e(TAG, "no result" + response.code());
+            }
+
+            @Override
+            public void onFailure(Call<MovieList> call, Throwable t) {
+                Log.i(TAG, t.getMessage());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.options_menu, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView = (SearchView) menu.findItem(R.id.search_view).getActionView();
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName())); // TODO
+        return true;
+    }
+}
